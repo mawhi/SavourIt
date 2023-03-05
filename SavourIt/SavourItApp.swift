4c8d0947b4818e574905a9c90920f4d8fe39c9eb
Mikhail Belogorodtsev <mawhi7@gmail.com>
2023-03-05 19:13:19 +0100

    [Project] Create project.

---
+++ SavourIt/SavourItApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavourItApp.swift
+//  SavourIt
+//
+//  Created by Mikhail Belogorodtsev on 05.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SavourItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
